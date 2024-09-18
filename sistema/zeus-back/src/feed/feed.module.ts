2640c0eac625e66a4882048851d27b52a47190f6
Filipe Augusto <filipeaugusto9074@gmail.com>
2024-09-18 19:47:50 -0300

    crud do feed de noticias

---
+++ sistema/zeus-back/src/feed/feed.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { FeedService } from './feed.service';
+import { FeedController } from './feed.controller';
+import { Feed } from '../entities/feed.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Feed])],
+  providers: [FeedService],
+  controllers: [FeedController],
+})
+export class FeedModule {}
---
+++ sistema/zeus-back/src/feed/feed.service.ts
@@ -0,0 +1,57 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Feed } from '../entities/feed.entity';
+
+@Injectable()
+export class FeedService {
+  constructor(
+    @InjectRepository(Feed) private repo: Repository<Feed>,
+  ) {}
+
+  create(number: number, block?: string) {
+    const feed = this.repo.create({
+      number,
+      block: block ?? null,
+    });
+
+    return this.repo.save(feed);
+  }
+
+  findAll() {
+    return this.repo.find();
+  }
+
+  async findOne(id: number) {
+    const feed = await this.repo.findOne({
+      where: { id },
+    });
+
+    if (!feed) {
+      throw new NotFoundException('Feed não encontrado.');
+    }
+
+    return feed;
+  }
+
+  findByNumber(number: number) {
+    return this.repo.find({ where: { number } });
+  }
+
+  async update(id: number, attrs: Partial<Feed>) {
+    const feed = await this.findOne(id);
+    if (!feed) {
+      throw new NotFoundException('Feed de notícia não encontrado.');
+    }
+    Object.assign(feed, attrs);
+    return this.repo.save(feed);
+  }
+
+  async remove(id: number) {
+    const feed = await this.findOne(id);
+    if (!feed) {
+      throw new NotFoundException('Apartamento não encontrado.');
+    }
+    return this.repo.remove(feed);
+  }
+}
