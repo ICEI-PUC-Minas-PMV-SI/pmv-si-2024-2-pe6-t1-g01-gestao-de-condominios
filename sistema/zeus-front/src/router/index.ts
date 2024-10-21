ed101da201481d1b8f556787cc32438c6f28744a
arturbani <arturbani@gmail.com>
2024-10-21 20:58:31 -0300

    crud de funcionarios

--- sistema/zeus-front/src/router/index.ts
+++ sistema/zeus-front/src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -6,59 +6,59 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: () => import('@/views/Site/Home.vue')
+      component: () => import('@/views/Site/Home.vue'),
     },
     {
       path: '/about',
       name: 'about',
-      component: () => import('@/views/Site/About.vue')
+      component: () => import('@/views/Site/About.vue'),
     },
     {
       path: '/login',
       name: 'login',
-      component: () => import('@/views/Site/Login.vue')
+      component: () => import('@/views/Site/Login.vue'),
     },
     {
       path: '/signup',
       name: 'signup',
-      component: () => import('@/views/Site/CreateAccount.vue')
+      component: () => import('@/views/Site/CreateAccount.vue'),
     },
     {
       path: '/feed-de-noticias',
       name: 'Feed de Notícias',
-      component: () => import('@/views/System/NewsFeed.vue')
+      component: () => import('@/views/System/NewsFeed.vue'),
     },
     {
       path: '/moradores',
       name: 'Moradores',
-      component: () => import('@/views/System/Residents.vue')
+      component: () => import('@/views/System/Residents.vue'),
     },
     {
       path: '/documentos',
       name: 'Documentos',
-      component: () => import('@/views/System/NewsFeed.vue')
+      component: () => import('@/views/System/NewsFeed.vue'),
     },
     {
       path: '/visitantes',
       name: 'Visitantes',
-      component: () => import('@/views/System/Visitors.vue')
+      component: () => import('@/views/System/Visitors.vue'),
     },
     {
       path: '/visitas',
       name: 'Visitas',
-      component: () => import('@/views/System/NewsFeed.vue')
+      component: () => import('@/views/System/NewsFeed.vue'),
     },
     {
       path: '/funcionarios',
       name: 'Funcionários',
-      component: () => import('@/views/System/NewsFeed.vue')
+      component: () => import('@/views/System/Employees.vue'),
     },
     {
       path: '/apartamentos',
       name: 'Apartamentos',
-      component: () => import('@/views/System/NewsFeed.vue')
-    }
-  ]
-})
+      component: () => import('@/views/System/NewsFeed.vue'),
+    },
+  ],
+});
 
-export default router
+export default router;
